bf2626bfb5b35937980a1148bdde52da9d82f9bb
CrazyMonkey0 <hubcio1765@gmail.com>
2025-03-24 13:15:16 +0100

    Enable CORS for frontend requests

--- app/main.py
+++ app/main.py
@@ -9,6 +9,14 @@ import os
 # Initialize application
 app = FastAPI()
 
+# Enable CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Load the pre-trained NLP 
 app.state.model_nlp, app.state.tokenizer_nlp = load_model_nlp()
