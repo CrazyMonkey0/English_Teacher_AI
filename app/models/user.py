82783ea16fa2b4228ac962bb1a4fb289ea6fcd4b
CrazyMonkey0 <hubcio1765@gmail.com>
2026-01-03 10:59:22 +0100

    feat(db): add User model for authentication

---
+++ app/models/user.py
@@ -0,0 +1,14 @@
+from fastapi_users.db import SQLAlchemyBaseUserTable
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import DeclarativeBase
+
+class Base(DeclarativeBase):
+    pass
+
+class User(SQLAlchemyBaseUserTable[int], Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String, nullable=True)
+    last_name = Column(String, nullable=True)
+    username = Column(String, unique=True, nullable=False)
