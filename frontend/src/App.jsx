86b6ed18f14eb22482a5f08607cdcf9935be5495
CrazyMonkey0 <hubcio1765@gmail.com>
2026-01-15 15:39:05 +0100

    feat(app): add routing and auth context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,22 @@
+import { Routes, Route } from "react-router-dom";
 import ChatBox from "./components/Chat/ChatBox";
+import Login from "./components/Auth/LoginForm";
+import { AuthProvider } from "./context/useAuth";
 import "./App.css";
 import { initCsrf } from "./services/csrf";
 
-initCsrf()
+initCsrf();
+
 function App() {
   return (
     <div className="App">
-      <ChatBox />
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<ChatBox />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </AuthProvider>
+
     </div>
   );
 }
